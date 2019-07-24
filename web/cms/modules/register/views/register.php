b096d9e3f734ebd959f408be612a8bdcb79c4074
nobodyinside <carry.chandra@gmail.com>
2019-07-24 11:35:34 +0700

    register fix link

--- web/cms/modules/register/views/register.php
+++ web/cms/modules/register/views/register.php
@@ -52,7 +52,7 @@
                 <button type="submit" class="btn btn-primary block full-width m-b">Register</button>
 
                 <p class="text-muted text-center"><small>Sudah Punya Akun ?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="<?php echo base_url();?>/login">Login</a>
+                <a class="btn btn-sm btn-white btn-block" href="<?php echo base_url();?>login">Login</a>
             </form>
             <p class="m-t"> <small>Smartelco Apps &copy; 2019</small> </p>
         </div>
